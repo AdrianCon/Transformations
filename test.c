f3e83361fdf6ebf2ed0f9e56d4d5dc96c8f4a2c6
Adrian <adriaan.contreras@gmail.com>
2019-09-27 12:59:48 -0500

    Add files via upload
    Transformacion verde

--- test.c
+++ test.c
@@ -1,46 +1,36 @@
-/*
- * GL04ModelTransform.cpp: Model Transform - Translation and Rotation
- * Transform primitives from their model spaces to world space.
- */
-#include <windows.h>  // for MS Windows
-#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include <windows.h>
+#include <GL/glut.h>
 
-/* Initialize OpenGL Graphics */
 void initGL() {
-   // Set "clearing" or background color
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black and opaque
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Color de fondo
 }
 
-/* Handler for window-repaint event. Call back when the window first appears and
-   whenever the window needs to be re-painted. */
 void display() {
    glClear(GL_COLOR_BUFFER_BIT);    // Clear the color buffer
    glMatrixMode(GL_MODELVIEW);      // To operate on Model-View matrix
    glLoadIdentity();                // Reset the model-view matrix
 
-   glTranslatef(-0.5f, 0.4f, 0.0f); // Translate left and up
-   glBegin(GL_QUADS);               // Each set of 4 vertices form a quad
-      glColor3f(1.0f, 0.0f, 0.0f);  // Red
-      glVertex2f(-0.3f, -0.3f);     // Define vertices in counter-clockwise (CCW) order
-      glVertex2f( 0.3f, -0.3f);     //  so that the normal (front-face) is facing you
-      glVertex2f( 0.3f,  0.3f);
-      glVertex2f(-0.3f,  0.3f);
-   glEnd();
-
-   glTranslatef(0.5f, -0.7f, 0.0f); // Translate right and down
-   glBegin(GL_QUADS);               // Each set of 4 vertices form a quad
-      glColor3f(0.0f, 1.0f, 0.0f); // Green
+   glTranslatef(0.0f, 0.0f, 0.0f);  //Origen
+   glBegin(GL_QUADS);               // Vertices
+      glColor3f(1.0f, 0.0f, 0.0f);  // Color rojo
       glVertex2f(-0.3f, -0.3f);
       glVertex2f( 0.3f, -0.3f);
       glVertex2f( 0.3f,  0.3f);
       glVertex2f(-0.3f,  0.3f);
    glEnd();
 
-   glFlush();   // Render now
+   glTranslatef(0.5f, -0.7f, 0.0f); // Translada a la derecha y abajo
+   glBegin(GL_QUADS);
+      glColor3f(0.0f, 1.0f, 0.0f);  //Verde
+      glVertex2f(-0.3f, -0.3f);
+      glVertex2f( 0.3f, -0.3f);
+      glVertex2f( 0.3f,  0.3f);
+      glVertex2f(-0.3f,  0.3f);
+   glEnd();
+
+   glFlush();   // Rende
 }
 
-/* Handler for window re-size event. Called back when the window first appears and
-   whenever the window is re-sized with its new width and height */
 void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
    // Compute aspect ratio of the new window
    if (height == 0) height = 1;                // To prevent divide by 0
@@ -61,16 +51,15 @@ void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integ
    }
 }
 
-/* Main function: GLUT runs as a console application starting at main() */
 int main(int argc, char** argv) {
    glutInit(&argc, argv);          // Initialize GLUT
-   glutInitWindowSize(640, 480);   // Set the window's initial width & height - non-square
-   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-   glutCreateWindow("Model Transform");  // Create window with the given title
-   glutDisplayFunc(display);       // Register callback handler for window re-paint event
-   glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-   initGL();                       // Our own OpenGL initialization
-   glutMainLoop();                 // Enter the infinite event-processing loop
+   glutInitWindowSize(640, 480);   // Tamaño de ventana
+   glutInitWindowPosition(50, 50); // Posicion Inicial
+   glutCreateWindow("Transformaciones");  // Crea ventana
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   initGL();
+   glutMainLoop();
    return 0;
 }
 
